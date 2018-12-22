8acd8ff96debf709664daf2507cedb104d3a4486
寺部　裕介 <yusuketerabe@terabuyuusukenoMacBook-Air.local>
2018-12-22 16:55:07 +0900

    first commit

---
+++ edit.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <herd>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="eidth=device-width,initisl-scale=1">
+
+      <title>MyNews</title>
+    </head>
+    <body>
+      <h1>myニュース作成画面</h1>
+    </body>
+</html>
+      
